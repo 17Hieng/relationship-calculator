b3126071bafc2d12abbc8afb20f671f47d96f085
Eii Chee Hieng <hieng2014@gmail.com>
2023-04-22 00:15:18 +0800

    Added relationship Constants

---
+++ src/relationship/Relationship.java
@@ -0,0 +1,49 @@
+package relationship;
+
+public class Relationship {
+    final static String GREATGREATGRANDFATHER = "Great Great Grandfather";
+    final static String GREATGREATGRANDMOTHER = "Great Great Grandmother";
+    final static String GREATGREATGRANDUNCLE = "Great Great Grand Uncle";
+    final static String GREATGREATGRANDAUNT = "Great Great Grand Aunt";
+
+    final static String GREATGRANDFATHER = "Great Grandfather";
+    final static String GREATGRANDMOTHER = "Great Grandmother";
+    final static String GREATGRANDUNCLE = "Great Grand Uncle";
+    final static String GREATGRANDAUNT = "Great Grand Aunt";
+
+    final static String GRANDFATHER = "Grandfather";
+    final static String GRANDMOTHER = "Grandmother";
+    final static String GRANDUNCLE = "Grand Uncle";
+    final static String GRANDAUNT = "Grand Aunt";
+
+    final static String FATHER = "Father";
+    final static String MOTHER = "Mother";
+
+    final static String ME = "Me";
+    final static String SISTER = "Sister";
+    final static String BROTHER = "Brother";
+    final static String FIRSTCOUSIN = "1st Cousin";
+    final static String SECONDCOUSIN = "2nd Cousin";
+    final static String THIRDCOUSIN = "3rd Cousin";
+    final static String FOURTHCOUSIN = "4th Cousin";
+
+    final static String CHILD = "Child";
+    final static String NIECE = "Niece";
+    final static String NEPHEW = "Nephew";
+
+    final static String GRANDCHILD = "Grand Child";
+    final static String GRANDNIECE = "Grand Niece";
+    final static String GRANDNEPHEW = "Grand Nephew";
+
+    final static String GREATCHILD = "Great Child";
+    final static String GREATNIECE = "Great Niece";
+    final static String GREATNEPHEW = "Great Nephew";
+
+    public static String calcalculateRelation(
+            String person1,
+            String person2
+    ){
+        
+        return ME;
+    }
+}
